b03b36d4e822d01bf74c71f09ba016ab3d113ff2
VoidMods <57848654+VoidMods@users.noreply.github.com>
2020-07-16 14:03:21 +0200

    Add stylesheet

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+file 'style.css'
+
+chat_theme '0xv-minimal' {
+    styleSheet = 'style.css',
+    msgTemplates = {
+        default = '<b>{0}</b><span>{1}</span>'
+    }
+}
+
+game 'common'
+fx_version 'adamant'
